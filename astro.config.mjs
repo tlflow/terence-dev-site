c6cf52a52d0d509b22053ea10cb67916c2839f57
Terence Flowers <tlflow33@gmail.com>
2023-08-23 18:22:14 -0400

    add animation styles

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,9 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+import { astroImageTools } from "astro-imagetools";
 
 import vue from "@astrojs/vue";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [vue()]
+  integrations: [vue(), astroImageTools],
 });
