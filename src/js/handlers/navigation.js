0b8956d21fb1725a45ecd2127910db78c9512983
Terence Flowers <tlflow33@gmail.com>
2023-10-11 16:20:11 -0400

    sitenav styling for large breakpoints

--- src/js/handlers/navigation.js
+++ src/js/handlers/navigation.js
@@ -2,32 +2,37 @@ const $_PAGEBODY = document.getElementById('page-body')
 const $_PANEL_MENU_BTNS = document.getElementsByClassName('menu-btn')
 const $_SITENAV_MENU = document.getElementById('main_menu')
 const $_CLOSE_BTN = document.getElementById('close_menu_btn')
+const $ARRAY_PANEL_MENU_BTNS = Array.from($_PANEL_MENU_BTNS)
+const $ARRAY_ALL_BTNS = [...$_PANEL_MENU_BTNS, $_CLOSE_BTN]
+
+function togglePanelBtnAnimation() {
+	requestAnimationFrame(() => {
+		$ARRAY_ALL_BTNS.forEach((btn) => {
+			if (btn.parentElement.classList.contains('header-panel')) {
+				btn.classList.toggle('-js--active')
+			}
+			return false
+		})
+	})
+}
 
 function toggleSiteMenu() {
+	togglePanelBtnAnimation()
 	if ($_SITENAV_MENU.dataset.status === '') {
 		$_SITENAV_MENU.dataset.status = 'active'
 		$_PAGEBODY.dataset.mainMenu = 'active'
-		requestAnimationFrame(toggleSiteMenu)
 		return
 	}
 	$_SITENAV_MENU.dataset.status = ''
 	$_PAGEBODY.dataset.mainMenu = ''
-	requestAnimationFrame(toggleSiteMenu)
 }
 
 function initSiteNavigation() {
-	const $ARRAY_PANEL_MENU_BTNS = Array.from($_PANEL_MENU_BTNS)
-
-	$ARRAY_PANEL_MENU_BTNS.forEach((element) => {
+	$ARRAY_ALL_BTNS.forEach((element) => {
 		element.addEventListener('click', (e) => {
 			e.preventDefault()
-			toggleSiteMenu()
+			requestAnimationFrame(toggleSiteMenu)
 		})
-	})
-
-	$_CLOSE_BTN.addEventListener('click', (e) => {
-		e.preventDefault()
-		toggleSiteMenu()
 	})
 }
 
